36ddf0b7f064e71ea47104509e4de48931d0f9a4
ericlin1230 <erichungyu.lin@mail.utoronto.ca>
2020-12-15 01:38:59 -0500

    Updated metastrem command

---
+++ commands/meta.js
@@ -0,0 +1,3 @@
+exports.run = async (client, message, args, ops) => {
+    message.channel.send("https://app.getmetastream.com/join/19f9ddfeb8bb53688d3ca64a7032c4b7bb8fa112b13cc42e5ae18b95a994ac59");
+}
